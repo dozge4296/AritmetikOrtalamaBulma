bc94603aa8c50ae9913eafa649d878c42b940237
dozge4296 <dozge4296@gmail.com>
2026-03-03 11:09:11 +0300

    AritmetikOrtalamaBulma
    C# ile yazılmış temel algoritma uygulaması. For döngüsü kullanılarak çoklu veri girişi alınmış ve toplam üzerinden ortalama hesaplama işlemi gerçekleştirilmiştir.

---
+++ Aritmetik ortalama bulma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aritmetik_ortalama_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double toplam = 0;
+            for (int i=1; i <= 5; i++)
+            {
+                Console.Write("Bir sayı giriniz:");
+                double not = Convert.ToDouble(Console.ReadLine());
+                toplam += not;
+
+            }
+            double ortalama = toplam / 5;
+            Console.WriteLine("ortalama" + ortalama);
+            Console.Read();
+        }
+    }
+}
